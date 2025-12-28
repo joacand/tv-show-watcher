b71c5b8c9e5553e8f78ca80782923f20f70fb774
joacand <tbsitu@gmail.com>
2025-12-28 15:58:43 +0100

    Fix

--- tv-show-watcher/app/search/page.tsx
+++ tv-show-watcher/app/search/page.tsx
@@ -5,7 +5,7 @@ import SearchGrid from "./SearchGrid";
 export default function Home() {
     return (
         <>
-            <h1 className="h-[26px] font-sans font-medium text-[20px] leading-[26px] flex-none order-1 mb-3">Search for TV shows</h1>
+            <h2 className="h-[26px] font-sans font-medium text-[20px] leading-[26px] flex-none order-1 mb-3">Search for TV shows</h2>
             <SearchGrid />
         </>
     );
