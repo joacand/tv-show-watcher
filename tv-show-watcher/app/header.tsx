885141d32d2228440490b9a0eb4736338f8b4976
joacand <tbsitu@gmail.com>
2026-01-10 11:22:54 +0100

    Logo adjustment

--- tv-show-watcher/app/header.tsx
+++ tv-show-watcher/app/header.tsx
@@ -5,19 +5,19 @@ import Image from 'next/image';
 const BASE = process.env.NEXT_PUBLIC_BASE_PATH ?? '';
 const LOGO_SRC = `${BASE}/logo.svg`;
 
-export default function Header({ className = "", children }: { className?: string, children?: React.ReactNode }) {
+export default function Header({ className = "" }: { className?: string }) {
 
     return (
-        <header className={`flex items-center px-[35px] gap-[100px] h-[147px] self-stretch flex-grow-0 ${className}`}>
+        <header className={`flex items-center px-10 gap-[100px] self-stretch flex-grow-0 ${className}`}>
             <Image loading="eager" src={LOGO_SRC} alt="Logo"
                 height={0}
                 width={0}
                 style={{
-                    width: '120px',
+                    width: '100px',
                     height: 'auto',
-                    filter: 'drop-shadow(0 0 1px white) drop-shadow(0 0 1px white)',
+                    filter: 'drop-shadow(0 0 1px white)',
                 }} />
-            <h1 className="font-sans font-medium text-[52px] leading-[46px]">TV Show Watcher</h1>
+            <h1 className="font-sans font-medium text-[52px] text-nowrap">TV Show Watcher</h1>
         </header>
     )
 }
